9aab61aa5da60964bd6703d659f88bdc1e14526a
Nathaliant <nathaliannt@gmail.com>
2018-11-04 12:34:57 -0200

    Entrega05_PrototipoNavegavel
    Entrega05

---
+++ Entrega05_PrototipoNavegavel/src/pages/genero/genero.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-genero',
+  templateUrl: 'genero.html',
+})
+export class GeneroPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad GeneroPage');
+  }
+
+}
